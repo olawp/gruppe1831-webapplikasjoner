b4136caa7b025f5dc23c71f0743ac2da828d49a2
yeti-programing <dankertsenalexander@gmail.com>
2020-12-01 11:36:26 +0100

    Further design and added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-      <nav className="navigationBar"><h3>FG</h3> <a className="loggInn" href="/logginn">LOGG INN</a><a href="/kontakt">Kontakt</a><a href="/fagartikler">Fagartikler</a><a href="/kontorer">Kontorer</a><a href="/">Hjem</a></nav>
+      <Navbar></Navbar>
       <header><h1>Velkommen til FG Rørleggerservice AS</h1></header>
-      <main></main>
-      <footer><p className="footerInfo">Orgnr: 007 007 007</p><p className="footerInfo">lg@lgror.no</p><p className="footerInfo">99 00 00 00</p></footer>
+      <main>
+        
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer><p className="footerInfo">Orgnr: 007 007 007</p><p className="footerInfo">lg@lgror.no</p><p className="footerInfo">99 00 00 00</p></footer>
+            </div>
+        )
+    }
+}
+
+export default Footer

5f9799a858922ea020426d6a129b1d886a0b129a
yeti-programing <dankertsenalexander@gmail.com>
2020-12-01 11:08:35 +0100

    Added some design to home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navigationBar"><h3>FG</h3> <a className="loggInn" href="/logginn">LOGG INN</a><a href="/kontakt">Kontakt</a><a href="/fagartikler">Fagartikler</a><a href="/kontorer">Kontorer</a><a href="/">Hjem</a></nav>
+      <header><h1>Velkommen til FG Rørleggerservice AS</h1></header>
+      <main></main>
+      <footer><p className="footerInfo">Orgnr: 007 007 007</p><p className="footerInfo">lg@lgror.no</p><p className="footerInfo">99 00 00 00</p></footer>
     </div>
   );
 }

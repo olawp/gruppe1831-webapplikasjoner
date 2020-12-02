3f7fe7bf9cae72a932a8fe0335f94d364e52012c
yeti-programing <dankertsenalexander@gmail.com>
2020-12-02 13:49:06 +0100

    Added feature to logg inn

--- frontend/src/components/pages/LoggInn.js
+++ frontend/src/components/pages/LoggInn.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import {Form, Input, Button} from '../../styled/style';
 
 export class LoggInn extends Component {
     render() {
         return (
             <div>
                 <header><h1>Logg inn</h1></header>
+                <main>
+                    <Form>
+                        <label>E-mail:</label>
+                        <br/>
+                        <Input type="email"></Input>
+                        <br/>
+                        <label>Password</label>
+                        <br/>
+                        <Input type="password"></Input>
+                        <br/>
+                        <Button>Logg inn</Button>
+                    </Form>
+                </main>
             </div>
         )
     }

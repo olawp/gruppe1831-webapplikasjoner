3d00518f779f5f13e374a573178429898d0ddfee
olawp <olawp1997@gmail.com>
2020-12-02 13:40:54 +0100

    minor typo

--- server/routes/article.js
+++ server/routes/article.js
@@ -4,7 +4,7 @@ import { articleController } from '../controllers/index.js';
 const router = express.Router();
 
 router.get('/:id', articleController.get);
-router.get('/:', articleController.list);
+router.get('/', articleController.list);
 router.post('/', articleController.create);
 router.put('/:id', articleController.update);
 router.delete('/:id', articleController.remove);

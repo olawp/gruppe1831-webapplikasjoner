0bd57af6c58498edba2410c7d197c18fc988525e
yeti-programing <dankertsenalexander@gmail.com>
2020-12-02 17:54:18 +0100

    Added getting articles from database

--- frontend/src/components/pages/Fagartikler.js
+++ frontend/src/components/pages/Fagartikler.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react'
 import ArtikkelList from '../artikkel/ArtikkelList';
 import {Button} from '../../styled/style';
+import Axios from 'axios';
 
 export class Fagartikler extends Component {
 
     state = {
-        artikkler: [
-            {
-                _id: 1,
-                title: "Bad og sortmugg",
-                ingress: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                content: "Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg Bad og sortmugg ",
-                category: "Bad",
-                author: "For Fatter"
-            },
-            {
-                _id: 2,
-                title: "Kjøkken og hygiene",
-                ingress: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                content: "Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene Kjøkken og hygiene ",
-                category: "Kjøkken",
-                author: "Ola Nordmann"
-            }
-        ]
+        artikkler: []
+    }
+
+    componentDidMount(){
+        Axios.get('http://localhost:5000/api/v1/articles')
+        .then(res => this.setState({ artikkler: res.data }))
+        .catch(
+            //TODO: ADD CATCH FOR WHEN SHIT GOES WRONG
+            )
     }
 
     render() {

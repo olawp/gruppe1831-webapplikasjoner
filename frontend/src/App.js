5b5c19c6b31423f70e109b54740a5ab885a994c6
olawp <olawp1997@gmail.com>
2020-12-03 14:43:33 +0100

    scuffed login, need to add a .env, because the api address is set in http.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,17 @@ import Navbar from './components/layout/Navbar';
 import Routes from './components/routes/routes';
 import Footer from './components/layout/Footer';
 import Style from './styled/style';
+import AuthProvider from './context/AuthProvider.jsx';
 
 function App() {
   return (
     <Style>
       <div className="App">
+      <AuthProvider>
         <Navbar></Navbar>
         <Routes/>
         <Footer></Footer>
+        </AuthProvider>
       </div>
     </Style>
   );

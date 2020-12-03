02b98a63fabc690684985a7234e66eb8ab894fe3
yeti-programing <dankertsenalexander@gmail.com>
2020-12-03 17:03:54 +0100

    added post to contact

--- frontend/src/components/pages/Kontakt.js
+++ frontend/src/components/pages/Kontakt.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import {Form, Input, Button} from '../../styled/style';
+import Axios from 'axios';
 
 export class Kontakt extends Component {
+
+    submitClicked = () => {
+        Axios.post('http://localhost:5000/api/v1/contact', {
+            name: document.getElementById("name").value,
+            email: document.getElementById("mail").value,
+            question: document.getElementById("question").value
+        })
+        .then(
+            document.getElementById("name").value = "",
+            document.getElementById("mail").value = "",
+            document.getElementById("question").value = ""
+        )
+        .catch(res => console.log(res));
+    }
+
     render() {
         return (
             <div>
@@ -9,22 +25,22 @@ export class Kontakt extends Component {
                 <main>
                     <Form>
                         <label>
+                            Ditt navn*:
+                        </label>
+                        <br/>
+                        <Input type="text" id="name"></Input>
+                        <br/>
+                        <label>
                             Din E-post*:
                         </label>
                         <br/>
-                        <Input type="email"></Input>
-                        <br/>
-                        <label>
-                            Ditt telefonnummer:
-                        </label>
-                        <br/>
-                        <Input type="tel"></Input>
+                        <Input type="email" id="mail"></Input>
                         <br/>
                         <label>
                             Ditt spørsmål*:
                         </label>
                         <br/>
-                        <Input type="text"></Input>
+                        <Input type="text" id="question"></Input>
                         <br/>
                         <Button type="submit">Send</Button>
                     </Form>

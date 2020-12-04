f72581e683203af05445d4dbb3364376dccac883
yeti-programing <dankertsenalexander@gmail.com>
2020-12-04 15:27:02 +0100

    updated detailed office and employee card

--- frontend/src/components/pages/DetaljertKontor.js
+++ frontend/src/components/pages/DetaljertKontor.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react'
 import AnsattCard from '../ansatt/AnsattCard';
 
+let lokasjon = window.location.href.split("/")[4];
+let nummer= window.location.href.split("/")[5];
+
 export class DetaljertKontor extends Component {
     render() {
         return (
             <div>
-                <header><h1>Kontor Rørlegger ´$nummer´</h1></header>
+                <header><h1>Kontor Rørlegger {nummer}</h1></header>
                 <main>
-                    <h2>Velkommen til Rørlegger ´$nummer´</h2>
-                    <p>
+                    <h2 style={{textAlign: "left"}}>Velkommen til Rørlegger {nummer}</h2>
+                    <p style={{textAlign: "left"}}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
-                    <h2>Våre ansatte</h2>
+                    <h2 style={{textAlign: "left"}}>Våre ansatte</h2>
                     <div className="ansattDiv">
                         <AnsattCard></AnsattCard>
                         <AnsattCard></AnsattCard>

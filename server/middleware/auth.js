a250e2792e6721c899adf71444e2f8a50012e781
olawp <olawp1997@gmail.com>
2020-12-04 14:01:35 +0100

    authentication stuff

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,29 +3,29 @@ import { userService } from '../services/index.js';
 import ErrorHandler from '../utils/errorHandler.js';
 import catchAsyncErrors from './catchAsync.js';
 
-export const isAuthenticated = catchAsyncErrors(async (req, res next) => {
-    let token;
-    if (req.cookies?.token) {
-        token = req.cookies.token;
-    }
+export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  let token;
+  if (req.cookies?.token) {
+    token = req.cookies.token;
+  }
 
-    if (!token) {
-        return next(new ErrorHandler('Ikke logget inn', 401))
-    }
+  if (!token) {
+    return next(new ErrorHandler('Ikke logget inn', 401));
+  }
 
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await userService.getUserById(decode.id);
-    if (!user) {
-        return next(new ErrorHandler('Brukeren eksisterer ikke', 404));
-    }
+  const decode = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await userService.getUserById(decode.id);
+  if (!user) {
+    return next(new ErrorHandler('Brukeren eksisterer ikke', 404));
+  }
 
-    req.user = user;
-    next();
+  req.user = user;
+  next();
 });
 
 export const isAuthorized = (...roles) => (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-        return next(new ErrorHandler('Ingen tilgang', 403));
-    }
-    next();
-}
+  if (!roles.includes(req.user.role)) {
+    return next(new ErrorHandler('Ingen tilgang', 403));
+  }
+  next();
+};

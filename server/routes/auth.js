8261ab0a25ccfabefb83ad5804c145ef32ae32ab
olawp <olawp1997@gmail.com>
2020-12-04 18:58:38 +0100

    removed register, for now. Posting to user does the same

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,6 @@ import { isAuthenticated } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.get('/me', isAuthenticated, authController.currentUser);

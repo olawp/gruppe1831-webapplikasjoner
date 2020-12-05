9ce148bc9fdb9d081a128d193215f1be6e293c33
yeti-programing <dankertsenalexander@gmail.com>
2020-12-06 00:05:24 +0100

    Finished mail, a copy will be sent to both the customer and the customer service employee

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -1,6 +1,7 @@
-import { contactService } from '../services/index.js';
+import { contactService, userService } from '../services/index.js';
 import ErrorHandler from '../utils/errorHandler.js';
 import catchAsyncErrors from '../middleware/catchAsync.js';
+import { sendMail } from '../utils/sendMail.js';
 
 export const get = catchAsyncErrors(async (req, res, next) => {
   const contact = await contactService.getContactById(req.params.id);
@@ -12,6 +13,20 @@ export const get = catchAsyncErrors(async (req, res, next) => {
 
 export const create = catchAsyncErrors(async (req, res, next) => {
   const contact = await contactService.createContact(req.body);
+  try {
+    await sendMail({
+      email: contact.email,
+      subject: 'FG Rørlegger Spørsmål',
+      message: `Hei ${contact.name},\n\nDu har nylig kontaktet oss med spørsmålet:\n${contact.question}\n\nVi vil svare deg på mail så snart vi er ledige.\n\nMvh.\nFG Rørlegger`,
+    });
+    await sendMail({
+      email: 'FGKontakt@FGRoerlegger.no',
+      subject: `Spørsmål fra kunde`,
+      message: `Kunden: ${contact.name} (${contact.email}) har stilt spørsmålet:\n${contact.question}`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.status(201).json(contact);
 });
 

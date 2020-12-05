79676ece583daf07ba2b5ac6f6fd938640c9b636
yeti-programing <dankertsenalexander@gmail.com>
2020-12-05 20:54:27 +0100

    Fixed a bug where Log Out button would not be hidden on smaller screens

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -47,7 +47,7 @@ const Navbar = () => {
                             <NavLoggInn style={{display: display}} className="loggInn" href="/logginn">LOGG INN</NavLoggInn>
                         )}
                         {isLoggedIn && (
-                            <NavLoggUt type="button" style={{display: display}} onClick={loggut}>LOGG UT</NavLoggUt>
+                            <NavLoggInn style={{display: display + " !important"}} onClick={loggut}>LOGG UT</NavLoggInn> //Begge er NavLoggInn, meb det trengs ingen NavLoggUt
                         )}
                         <NavButtons style={{display: display}} className={activePage("kontakt")} href="/kontakt">Kontakt</NavButtons>
                         <NavButtons style={{display: display}} className={activePage("fagartikler", "artikkel")} href="/fagartikler">Fagartikler</NavButtons>

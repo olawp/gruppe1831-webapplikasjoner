11930236154b37791913c9c700aab23ed64509cd
olawp <olawp1997@gmail.com>
2020-12-06 21:29:53 +0100

    posting articles works

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,12 +1,17 @@
 import express from 'express';
 // eslint-disable-next-line import/named
 import { articleController } from '../controllers/index.js';
+import { isAuthenticated, isAuthorized } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/:id', articleController.get);
 router.get('/', articleController.list);
-router.post('/', articleController.create);
+router.post(
+  '/',
+  [isAuthenticated, isAuthorized('admin')],
+  articleController.create
+);
 router.put('/:id', articleController.update);
 router.delete('/:id', articleController.remove);
 

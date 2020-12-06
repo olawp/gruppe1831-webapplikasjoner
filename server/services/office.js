29e160c7a367bbb7010f46a8be5e4cc628dc418d
yeti-programing <dankertsenalexander@gmail.com>
2020-12-06 20:31:39 +0100

    deleted office

--- server/services/office.js
+++
@@ -1,7 +0,0 @@
-import Office from '../models/office.js';
-
-export const listOffice = async () => Office.find();
-
-export const getOfficeById = async (id) => Office.findById(id);
-
-export const createOffice = async (data) => Office.create(data);

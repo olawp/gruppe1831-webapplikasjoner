1f09565f0161e8abe274f64bed3b41c2eded8d72
yeti-programing <dankertsenalexander@gmail.com>
2020-12-06 20:33:25 +0100

    removed office import

--- server/services/index.js
+++ server/services/index.js
@@ -1,5 +1,4 @@
 export * as userService from './user.js';
-export * as officeService from './office.js';
 export * as categoryService from './category.js';
 export * as contactService from './contact.js';
 export * as imageService from './image.js';

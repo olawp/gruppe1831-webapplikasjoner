11867ca8048acd5ab78c1b395f8ad12643dde1c3
olawp <olawp1997@gmail.com>
2020-12-08 21:09:18 +0100

    this too

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,12 +8,10 @@ import express from 'express';
 // eslint-disable-next-line import/named
 import { authController } from '../controllers/index.js';
 import { isAuthenticated } from '../middleware/auth.js';
-import { loginSchema } from '../schemas/user.js';
-import { validation } from '../middleware/validation.js';
 
 const router = express.Router();
 
-router.post('/login', validation(loginSchema), authController.login);
+router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.get('/me', isAuthenticated, authController.currentUser);
 

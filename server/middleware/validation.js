309fbca0c247f88a622cd696a4abc66133de81bd
olawp <olawp1997@gmail.com>
2020-12-08 21:07:49 +0100

    i done fuked something

--- server/middleware/validation.js
+++
@@ -1,6 +0,0 @@
-import { validate } from '../utils/validation.js';
-
-export const validation = (schema) => async (req, res, next) => {
-  console.log(await validate(schema, req.body));
-  next();
-};
--- server/utils/validation.js
+++
@@ -1,7 +0,0 @@
-export const validate = async (schema, input) => {
-  try {
-    return await schema.validateAsync({ ...input });
-  } catch (error) {
-    return error;
-  }
-};
